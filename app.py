c519ce1abae95af923185fe32b382059c727a0b0
Diego-Machado-00 <61296868+Diego-Machado-00@users.noreply.github.com>
2023-12-02 18:54:38 +0000

    V3

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def predict_api():
     return jsonify({'prediction': prediction}) 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", debug=True)
