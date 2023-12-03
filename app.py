78472650f40f64700984a41c5aac6a2ff64c8606
Diego-Machado-00 <61296868+Diego-Machado-00@users.noreply.github.com>
2023-12-03 03:35:48 +0000

    V7

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import pickle
+import numpy as np
 
 app = Flask(__name__)
 model = pickle.load(open("models/model_perceptron.pkl", "rb"))
@@ -10,7 +11,23 @@ def home():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    return render_template("index.html", predictions = 2)
+    age = request.form.get("Age")
+    sex = request.form.get("Sex")
+    physActivity = request.form.get("PhysActivity")
+    fruits = request.form.get("Fruits")
+    veggies = request.form.get("Veggies")
+    hvyAlcoholConsump = request.form.get("HvyAlcoholConsump")
+    smoker = request.form.get("Smoker")
+    highBP = request.form.get("HighBP")
+    highChol = request.form.get("HighChol")
+    bMI = request.form.get("BMI")
+    genHlth = request.form.get("GenHlth")
+    physHlthad = request.form.get("PhysHlth")
+    diffWalk = request.form.get("DiffWalk")
+    heartDiseaseorAttack = request.form.get("HeartDiseaseorAttack")
+    arr = np.array([[age, sex, physActivity, fruits, veggies, hvyAlcoholConsump, smoker, highBP, highChol, bMI, genHlth, physHlthad, diffWalk, heartDiseaseorAttack]])
+    pred = model.predict(arr)
+    return render_template("index.html", predictions = pred)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
